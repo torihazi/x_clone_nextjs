4173d42c93681114be70da9ba3f5e574e5f64355
torihazi <lovebarusa19981225@icloud.com>
2025-01-21 19:57:22 +0900

    side-barのview完成

--- components/layouts/main-layout.tsx
+++ components/layouts/main-layout.tsx
@@ -1,19 +1,16 @@
 import NavBar from "./nav-bar";
 import SubSideBar from "./sub-side-bar";
+
 export default function MainLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex h-screen">
-      <div className="">
-        <NavBar />
-      </div>
+    <div className="flex h-screen lg:mx-24">
+      <NavBar />
       <div className="grow">{children}</div>
-      <div className="">
-        <SubSideBar />
-      </div>
+      <SubSideBar />
     </div>
   );
 }
--- components/layouts/nav-bar.tsx
+++ components/layouts/nav-bar.tsx
@@ -1,3 +1,72 @@
+import {
+  Home,
+  Search,
+  Bell,
+  MessageCircle,
+  Users,
+  User,
+  MoreHorizontal,
+} from "lucide-react";
+import Image from "next/image";
+import { useRouter } from "next/router";
+
+import { Button } from "../ui/button";
+
 export default function NavBar() {
-  return <div>NavBar</div>;
+  const router = useRouter();
+  const columns: { icon: React.ReactNode; title: string; path: string }[] = [
+    { icon: <Home className="size-7" />, title: "Home", path: "/" },
+    { icon: <Search className="size-7" />, title: "Explore", path: "/explore" },
+    {
+      icon: <Bell className="size-7" />,
+      title: "Notifications",
+      path: "/notifications",
+    },
+    {
+      icon: <MessageCircle className="size-7" />,
+      title: "Messages",
+      path: "/messages",
+    },
+    {
+      icon: <Users className="size-7" />,
+      title: "Communities",
+      path: "/communities",
+    },
+    { icon: <User className="size-7" />, title: "Profile", path: "/profile" },
+    {
+      icon: <MoreHorizontal className="size-7" />,
+      title: "Settings",
+      path: "/settings",
+    },
+  ];
+
+  return (
+    <div className="flex h-full flex-col justify-between px-2 lg:w-[225px]">
+      <div className="flex flex-col items-start gap-5">
+        <Button variant="ghost" className="rounded-full p-2">
+          <Image src="/logo-white.png" alt="logo" width={30} height={30} />
+        </Button>
+        {columns.map((column) => (
+          <Button
+            variant="ghost"
+            className="text-md gap-4 rounded-full p-1 font-bold"
+            key={column.title}
+            onClick={() => router.push(column.path)}
+          >
+            {column.icon}
+            {column.title}
+          </Button>
+        ))}
+        <Button className="text-md w-full gap-4 rounded-full px-3 font-bold">
+          POST
+        </Button>
+      </div>
+      <div className="flex flex-col items-start justify-start">
+        <Button variant="ghost" className="gap-4 rounded-full p-1">
+          <User className="size-7" />
+          hgoehgoe
+        </Button>
+      </div>
+    </div>
+  );
 }
